308087574e8a4ef4983f15274e666db764d0a03a
fady emil <fady46t45f@gmail.com>
2024-09-12 18:47:22 +0300

    colors App

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color brandColors1 = Color(0xff92A3FD);
+  static const Color brandColors2 = Color(0xff9DCEFF);
+
+  static const Color borderColor = Color(0xffF7F8F8);
+
+  static const Color blackColor = Color(0xff1D1617);
+  static const Color whiteColor = Color(0xffFFFFFF);
+
+  static const Color grayColor1 = Color(0xff7B6F72);
+  static const Color grayColor2 = Color(0xffADA4A5);
+  static const Color grayColor3 = Color(0xffDDDADA);
+
+  static const Color secondaryColor1 = Color(0xffC58BF2);
+  static const Color secondaryColor2 = Color(0xffEEA4CE);
+}
