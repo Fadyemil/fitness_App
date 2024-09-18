ab87dee0da2a4d8eeb30cabc76e390e6c27ee0a5
fady emil <fady46t45f@gmail.com>
2024-09-18 18:52:59 +0300

    auth Repo part 2

--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -1,38 +1,18 @@
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
-import 'package:fitness_app/core/cache/cach_helper.dart';
+// import 'package:dio/dio.dart';
+// import 'package:fitness_app/core/cache/cach_helper.dart';
 import 'package:fitness_app/core/error/exceptions.dart';
 import 'package:fitness_app/features/auth/data/api/api_service.dart';
 import 'package:fitness_app/features/auth/data/api/end_point.dart';
-import 'package:fitness_app/features/auth/data/models/sign_in_model.dart';
+// import 'package:fitness_app/features/auth/data/models/sign_in_model.dart';
 import 'package:fitness_app/features/auth/data/models/sign_up_model.dart';
 import 'package:fitness_app/features/auth/domain/entites/body_response_login.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
+// import 'package:jwt_decoder/jwt_decoder.dart';
 
-class AuthRepo {
+abstract class AuthRepo {
   final ApiService apiService = ApiService();
 
-  Future<Either<String, SignInModel>> signInRepo(
-      {required BodyResponseLogin bodyResponseLogin}) async {
-    try {
-      final Response response = await apiService.post(
-        url: EndPoint.signIn,
-        body: {
-          ApiKey.email: bodyResponseLogin.email,
-          ApiKey.password: bodyResponseLogin.password,
-        },
-      );
-      final user = SignInModel.fromJson(response.data);
-      final decodedToken = JwtDecoder.decode(user.token);
-      CacheHelper().saveData(key: ApiKey.token, value: user.token);
-      CacheHelper().saveData(key: ApiKey.id, value: decodedToken[ApiKey.id]);
-      return Right(user);
-    } on ServerException catch (e) {
-      return Left(e.errModel.errorMessage);
-    } catch (e) {
-      return Left(e.toString()); // Catch other exceptions
-    }
-  }
+  Future<Either<String, BodyResponseLogin>> signInRepo();
 
   Future<Either<String, SignUpModel>> signUPRepo(
       {required String email,
